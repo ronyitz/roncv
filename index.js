6b84224e8e2e6e1cf4646331de32c53f2cc4c4cb
ronyitz <49119539+ronyitz@users.noreply.github.com>
2021-05-05 08:53:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,15 @@
-function load(){
-
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        document.getElementById("home").style.height = window.screen.availHeight+"px";
-        document.getElementsByClassName("centered")[0].style.top = window.screen.availHeight*0.5+"px";
-        document.getElementsByClassName("centered")[0].style.fontSize = "30px";
-        document.body.removeAttribute("style");
+function load() {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        document.getElementById("home").style.height = window.screen.availHeight + "px";
         document.body.style.fontFamily = "Arial";
-    }else{
-        document.body.removeAttribute("style");
     }
+    document.body.removeAttribute("style");
 }
 
-
-function homeNav(){
-    $('html,body').animate({
-        scrollTop: $("body").offset().top},
-        'slow');
+function navigate(val) {
+    $('html,body').animate({ scrollTop: $('#' + val).offset().top }, 'slow');
 }
 
-
-function aboutNav(){
-    $('html,body').animate({
-        scrollTop: $("#about").offset().top},
-        'slow');
-}
-
-function experienceNav(){
-    $('html,body').animate({
-        scrollTop: $("#experience").offset().top},
-        'slow');
-}
-
-
-function contactNav(){
-    $('html,body').animate({
-        scrollTop: $("#contact").offset().top},
-        'slow');
-}
-
-function tolinkedin(){
+function tolinkedin() {
     location.replace('https://www.linkedin.com/in/ron-yitzhak1990');
 }
